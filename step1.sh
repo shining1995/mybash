f4efe1b6a2bdb5987444281765dd4415ae518fe2
shining1995 <32747489+shining1995@users.noreply.github.com>
2020-03-28 16:23:53 +0800

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -1,5 +1,5 @@
 sudo yum install -y epel-release 
-sudo yum install vim neovim @Development wget lrzsz htop lsof docker
+sudo yum install vim neovim @Development wget lrzsz htop lsof docker xclip
 sudo yum install -y zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
